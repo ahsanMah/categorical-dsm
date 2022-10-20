c71b7e5abfba8129bca5b057dfe82d85017379fa
ahsanMah <mahmood.ahsan@outlook.com>
2022-10-20 22:37:12 +0000

    Fixed bug in loss calculation

--- losses.py
+++ losses.py
@@ -19,13 +19,14 @@ def gumbel_grad(x_sample, class_logits, tau, K):
 
     return grad
 
-
 def categorical_dsm_loss(x_logit, x_noisy, scores, tau):
     """
     x_logit: Logit probs of original sample
     x_noisy: probaility tensor of noisy image
     """
+    batch_sz = x_logit.shape[0]
     targets = log_concrete_grad(x_noisy, x_logit, tau=tau)
-    loss = 0.5 * (scores - targets) ** 2
+    loss = (scores - targets) ** 2
+    loss = 0.5 * torch.sum(loss.reshape(batch_sz, -1), dim=-1)
 
     return torch.mean(loss)
