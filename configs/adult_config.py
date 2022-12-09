94b10e007597d7ea38eb1c58ba34b8b4081eb599
ahsanMah <mahmood.ahsan@outlook.com>
2022-12-09 15:38:05 +0000

    Updated config

--- configs/adult_config.py
+++ configs/adult_config.py
@@ -6,8 +6,8 @@ def get_config():
     config = ml_collections.ConfigDict()
     # training
     config.training = training = ml_collections.ConfigDict()
-    config.training.batch_size = 256
-    training.n_epochs = 100
+    config.training.batch_size = 128
+    training.n_epochs = 1000
     training.log_freq = 10
     training.eval_freq = 50
     training.checkpoint_freq = 200
@@ -27,19 +27,22 @@ def get_config():
     config.model = model = ml_collections.ConfigDict()
     model.name = "tab-mlp"
     model.tau_min = 2.0
-    model.tau_max = 10
+    model.tau_max = 20
+    model.sigma_min = 1e-1
+    model.sigma_max = 1.0
     model.num_scales = 10
     model.estimate_noise = False
-    model.ndims = 128
-    model.time_embedding_size = 32
+    model.ndims = 256
+    model.time_embedding_size = 64
     model.layers = 4
-    model.dropout = 0.1
+    model.dropout = 0.2
     model.act = "gelu"
     model.embedding_type = "fourier"
+    model.cond_type = "linear"
 
     # optimization
     config.optim = optim = ml_collections.ConfigDict()
-    optim.weight_decay = 1e-4
+    optim.weight_decay = 0.0
     optim.optimizer = "AdamW"
     optim.lr = 2e-4
     optim.grad_clip = 1.0
