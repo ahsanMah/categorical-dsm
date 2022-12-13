19b5fb5b023f8f0186d4888cc08990d8d7af3b8c
ahsanMah <mahmood.ahsan@outlook.com>
2022-12-13 16:30:02 +0000

    Pushing updated config

--- configs/adult_config.py
+++ configs/adult_config.py
@@ -1,16 +1,17 @@
 import ml_collections
 import torch
-
+import math
 
 def get_config():
     config = ml_collections.ConfigDict()
     # training
     config.training = training = ml_collections.ConfigDict()
     config.training.batch_size = 128
-    training.n_epochs = 1000
-    training.log_freq = 10
-    training.eval_freq = 50
-    training.checkpoint_freq = 200
+    training.n_steps = 1000000
+    # training.n_epochs = 1000
+    training.log_freq = 100
+    training.eval_freq = 100
+    training.checkpoint_freq = 2000
     training.snapshot_freq = 10000
 
     # evaluation
@@ -25,7 +26,7 @@ def get_config():
 
     # model
     config.model = model = ml_collections.ConfigDict()
-    model.name = "tab-mlp"
+    model.name = "tab-resnet"
     model.tau_min = 2.0
     model.tau_max = 20
     model.sigma_min = 1e-1
@@ -35,10 +36,10 @@ def get_config():
     model.ndims = 256
     model.time_embedding_size = 64
     model.layers = 4
-    model.dropout = 0.2
+    model.dropout = 0.0
     model.act = "gelu"
     model.embedding_type = "fourier"
-    model.cond_type = "linear"
+    model.ema_rate = 0.0
 
     # optimization
     config.optim = optim = ml_collections.ConfigDict()
