0b36225958f82b0c19f3bfce3061f10fc092f515
ahsanMah <mahmood.ahsan@outlook.com>
2022-12-22 17:30:56 +0000

    Changed resnext to use groupnorm

--- models/layers.py
+++ models/layers.py
@@ -11,7 +11,6 @@ activations = {
     "gelu": F.gelu,
     "selu": F.selu,
     "relu": F.relu,
-    "prelu": F.prelu,
     "softplus": F.softplus,
 }
 
@@ -66,9 +65,9 @@ class FiLMBlock(nn.Module):
     def forward(self, X, time_emb):
         gamma, beta = torch.split(self.dense(time_emb), (self.c, self.c), dim=1)
         # print(gamma[:,:,None, None].shape)
-        
+
         if self.img_input:
-            return X *gamma[:, :, None, None] + beta[:, :, None, None]
+            return X * gamma[:, :, None, None] + beta[:, :, None, None]
 
         return X * gamma + beta
 
@@ -88,12 +87,11 @@ class ResNeXtBlock(nn.Module):
     ):
         super().__init__()
 
-        # !FIXME: Get this from config
         self.act = get_act(act)
 
         bot_channels = int(round(num_channels * bot_mul))
         self.conv1 = nn.Conv2d(num_channels, bot_channels, kernel_size=1, stride=1)
-        self.norm1 = nn.InstanceNorm2d(bot_channels)
+        self.norm1 = nn.GroupNorm(num_groups=8, num_channels=bot_channels)
 
         self.conv2 = nn.Conv2d(
             bot_channels,
@@ -103,22 +101,22 @@ class ResNeXtBlock(nn.Module):
             padding=1,
             groups=bot_channels // groups,
         )
-        self.norm2 = nn.InstanceNorm2d(bot_channels)
+        self.norm2 = nn.GroupNorm(num_groups=8, num_channels=bot_channels)
 
         self.conv3 = nn.Conv2d(bot_channels, num_channels, kernel_size=1, stride=1)
-        self.norm3 = nn.InstanceNorm2d(num_channels)
+        self.norm3 = nn.GroupNorm(num_groups=8, num_channels=num_channels)
 
         if use_1x1conv:
             self.conv4 = nn.Conv2d(
                 num_channels, num_channels, kernel_size=1, stride=strides
             )
-            self.norm4 = nn.InstanceNorm2d(num_channels)
+            self.norm4 = nn.GroupNorm(num_groups=8, num_channels=num_channels)
         else:
             self.conv4 = None
 
-        self.film1 = FiLMBlock(time_emb_size, bot_channels)
-        self.film2 = FiLMBlock(time_emb_size, bot_channels)
-        self.film3 = FiLMBlock(time_emb_size, num_channels)
+        self.film1 = FiLMBlock(time_emb_size, bot_channels, img_input=True)
+        self.film2 = FiLMBlock(time_emb_size, bot_channels, img_input=True)
+        self.film3 = FiLMBlock(time_emb_size, num_channels, img_input=True)
 
     def forward(self, X, time_emb):
 
