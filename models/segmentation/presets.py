3fba914d0caa4d51423dd3ab26be7c1c800769c8
ahsanMah <mahmood.ahsan@outlook.com>
2023-02-07 16:48:18 +0000

    + Addeed logit transformation to seg transforms + Returns a img,target concatenated tensor

--- models/segmentation/presets.py
+++ models/segmentation/presets.py
@@ -62,7 +62,8 @@ class SegmentationTrain:
         crop_size,
         hflip_prob=0.5,
         mean=(0.485, 0.456, 0.406),
-        std=(0.229, 0.224, 0.225)
+        std=(0.229, 0.224, 0.225),
+        to_logits=False,
     ):
         min_size = int(0.5 * base_size)
         max_size = int(2.0 * base_size)
@@ -81,13 +82,25 @@ class SegmentationTrain:
             ]
         )
         self.transforms = T.Compose(trans)
-        self.to_onehot = partial(F.one_hot, num_classes=21)
+        # self.to_onehot = partial(F.one_hot, num_classes=21)
+        def build_one_hot_transform(to_logits=to_logits):
+            # print('TO LOGITS:', to_logits)
+            # @torch.jit.script
+            def to_onehot(target):
+                target = F.one_hot(target,  num_classes=21).squeeze().float()
+                target = target.permute(2, 0, 1)
+                if to_logits:
+                    target = torch.log(torch.clamp(target, min=1e-5, max=1.0))
+                
+                return target
+            return to_onehot
+        
+        self.to_onehot = build_one_hot_transform()
 
     def __call__(self, img, target):
         img, target = self.transforms(img, target)
         target[target == 255] = 0
         target = self.to_onehot(target)
-        target = target.permute(2, 0, 1).squeeze().float()
         # print(img.shape, target.shape)
         img = torch.cat((img, target), dim=0)
         return img, 0
@@ -98,26 +111,39 @@ class SegmentationEval:
         *,
         out_size,
         mean=(0.485, 0.456, 0.406),
-        std=(0.229, 0.224, 0.225)
+        std=(0.229, 0.224, 0.225),
+        to_logits=False,
     ):
 
         self.out_sz = out_size
         
         self.transforms = T.Compose(
             [
-                T.RandomResize(self.out_sz, self.out_sz),
+                T.Resize(self.out_sz, self.out_sz),
                 T.PILToTensor(),
                 T.ConvertImageDtype(torch.float),
                 T.Normalize(mean=mean, std=std),
             ]
         )
-        self.transforms = T.Compose(trans)
-        self.to_onehot = partial(F.one_hot, num_classes=21)
+        # self.to_onehot = partial(F.one_hot, num_classes=21)
+        
+        def build_one_hot_transform(to_logits=to_logits):
+            def to_onehot(target):
+                target = F.one_hot(target,  num_classes=21).squeeze().float()
+                target = target.permute(2, 0, 1)
+                if to_logits:
+                    target = torch.log(torch.clamp(target, min=1e-5, max=1.0))
+                
+                return target
+            return to_onehot
+        
+        self.to_onehot = build_one_hot_transform()
+
 
     def __call__(self, img, target):
         img, target = self.transforms(img, target)
         target[target == 255] = 0
         target = self.to_onehot(target)
-        target = target.permute(2, 0, 1).squeeze().float()
-        # print(img.shape, target.shape)
-        return img, target
+        img = torch.cat((img, target), dim=0)
+        
+        return img, 0
